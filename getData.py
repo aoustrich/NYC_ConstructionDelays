fced033b86aea7357d24419a8516eee91c720dc7
Aaron Oustrich <aaron.oustrich@gmail.com>
2023-11-27 12:20:29 -0700

    Phase 01

---
+++ getData.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import requests
+import json
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+from creds import *
+
+from requests.auth import HTTPBasicAuth
+basic = HTTPBasicAuth(nyc_api_key, nyc_api_secret)
+r = requests.get('https://data.cityofnewyork.us/resource/s7yh-frbm.json?$limit=497727', auth=basic)
+df = pd.read_json(r.text)
+
+df = df.drop_duplicates(subset = ['project_id','seq_number'],keep='last').reset_index(drop=True)
+# print(df.shape)
+df.to_csv("data/nycMilestones.csv",index=False) 
