0f9bf08647541c94773db1c8c96926e83ab687c1
Aaron Oustrich <aaron.oustrich@gmail.com>
2023-12-07 18:58:00 -0700

    Add Budget Data

--- getData.py
+++ getData.py
@@ -7,10 +7,17 @@ from creds import *
 
 from requests.auth import HTTPBasicAuth
 basic = HTTPBasicAuth(nyc_api_key, nyc_api_secret)
-r = requests.get('https://data.cityofnewyork.us/resource/s7yh-frbm.json?$limit=497727', auth=basic)
-df = pd.read_json(r.text)
+milestones_request = requests.get('https://data.cityofnewyork.us/resource/s7yh-frbm.json?$limit=497727', auth=basic)
+milestones = pd.read_json(milestones_request.text)
 
-df = df.drop_duplicates(subset = ['project_id','seq_number'],keep='last').reset_index(drop=True)
+milestones = milestones.drop_duplicates(subset = ['project_id','seq_number'],keep='last').reset_index(drop=True)
 # print(df.shape)
-df.dropna(axis = 0, subset=['orig_start_date','orig_end_date','task_start_date','task_end_date'],inplace=True)
-df.to_csv("data/nycMilestones.csv",index=False) 
+milestones.dropna(axis = 0, subset=['orig_start_date','orig_end_date','task_start_date','task_end_date'],inplace=True)
+milestones.to_csv("data/nycMilestones.csv",index=False) 
+
+
+# Do the same for the budget dataset
+budget_request = requests.get('https://data.cityofnewyork.us/resource/wa2y-rh4b.json?$limit=72437', auth=basic)
+budget = pd.read_json(budget_request.text)
+
+budget.to_csv('data/budget.csv', index=False)
